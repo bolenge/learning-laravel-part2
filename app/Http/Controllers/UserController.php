e9c67ec177c777200fd101eceaa64c2c261acaa3
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-01-09 10:41:29 +0100

    Creation de requests for create and update user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Repositories\UserRepository;
+use App\Http\Requests\UserCreateRequest;
 
 class UserController extends Controller
 {
