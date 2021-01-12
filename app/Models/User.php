3a65268431dd32f31ef9392694ad1673fceb6e28
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-01-12 12:02:16 +0100

    Creation de UserRepository extends ResourceRepository

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'admin'
     ];
 
     /**
@@ -40,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = \bcrypt($password);
+    }
 }
--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -2,51 +2,10 @@
     namespace App\Repositories;
 
     use App\Models\User;
-    class UserRepository {
-
-        protected $user;
+    class UserRepository extends ResourceRepository {
 
         public function __construct(User $user)
         {
-            $this->user = $user;
-        }
-
-        public function save(User $user, Array $inputs)
-        {
-            $user->name = $inputs['name'];
-            $user->email = $inputs['email'];
-            $user->admin = isset($inputs['admin']);
-
-            $user->save();
-        }
-
-        public function getPaginate($n)
-        {
-            return $this->user->paginate($n);
-        }
-        
-        public function store(array $inputs)
-        {
-            $user = new $this->user;
-            $user->password = \bcrypt($inputs['password']);
-
-            $this->save($user, $inputs);
-
-            return $user;
-        }
-
-        public function getById($id)
-        {
-            return $this->user->findOrFail($id);
-        }
-
-        public function update($id, Array $inputs)
-        {
-            $this->save($this->getById($id), $inputs);
-        }
-
-        public function destroy($id)
-        {
-            $this->getById($id)->delete();
+            $this->model = $user;
         }
     }
