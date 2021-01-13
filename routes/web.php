e54300e76588eeed081d3b92a35f5f7ff91e0bb9
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-01-13 14:42:55 +0100

    Installation de certains modules

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 });
 
 Route::resource('user', 'UserController');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
---
+++ test.js
@@ -0,0 +1,5 @@
+var myArray = [3, 1, 5, 10, 4, 2];
+myArray.sort(function (a, b) {
+    return a - b;
+});
+console.log(myArray);
