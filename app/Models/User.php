e5f72fb6268713596d59e6350eae37e763771440
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-01-16 03:24:02 +0100

    Updating Model/User

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,8 +42,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function setPasswordAttribute($password)
-    {
-        $this->attributes['password'] = \bcrypt($password);
-    }
+    // public function setPasswordAttribute($password)
+    // {
+    //     $this->attributes['password'] = \bcrypt($password);
+    // }
 }
