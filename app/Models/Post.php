0caf4dc2d33460f9eeb3a1d5e7e40b45dccb29a4
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-01-27 13:38:43 +0100

    Mise en relation du model User et Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $faillible = ['title', 'content', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     // {
     //     $this->attributes['password'] = \bcrypt($password);
     // }
+
+    public function posts()
+    {
+        return $this->hasMany('App\Models\Post');
+    }
 }
