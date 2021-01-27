d27d36aa215862f87c8316482153ddb0ebb3ace4
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-01-27 14:46:54 +0100

    Ajout de la route du blog

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -13,7 +13,7 @@ class PostRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class PostRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required|max:80',
+            'content' => 'required'
         ];
     }
 }
--- resources/views/templates/layout.blade.php
+++ resources/views/templates/layout.blade.php
@@ -17,9 +17,11 @@
                 <div class="navbar-nav">
                     <a class="nav-item nav-link {{ get_menu_active('home', $active) }}" href="/">Accueil <span class="sr-only">(current)</span></a>
                     <a class="nav-item nav-link {{ get_menu_active('users', $active) }}" href="/user">Utilisateurs</a>
+                    <a class="nav-item nav-link {{ get_menu_active('blog', $active) }}" href="/post">Blog</a>
                 </div>
             </div>
         </nav>
+
         @yield('content')
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
