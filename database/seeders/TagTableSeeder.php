8fda5d8cc969bfcf1aa8bd936705a4d4d81cd4fa
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-02-06 08:41:32 +0100

    Creation de la table TagTableSeeder

---
+++ database/seeders/TagTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use DB;
+
+class TagTableSeeder extends Seeder
+{
+    private function randDate()
+    {
+        return Carbon::createFromDate(null, \rand(1,12), \rand(1,28));
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('tags')->delete();
+
+        for ($i=0; $i < 20; ++$i) { 
+            $date = $this->randDate();
+
+            DB::table('tags')->insert([
+                'tag' => 'tag'.$i,
+                'tag_url' => 'tag'.$i,
+                'created_at' => $date,
+                'updated_at' => $date,
+            ]);
+        }
+    }
+}
